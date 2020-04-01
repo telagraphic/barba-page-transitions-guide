b8367779e19ddc0afbf3533af6414c1fadfced2f
telagraphic <telagraphic@gmail.com>
2020-04-01 12:49:05 -0400

    🖥 FRONT-END GENERAL: setup simple fade transition with gsap

--- js/app.js
+++ js/app.js
@@ -1,1 +1,37 @@
-console.log("working");
+barba.init({
+  transitions: [{
+    name: 'fade',
+    leave({current, next, trigger}) {
+      return new Promise(resolve => {
+
+        const timeline = gsap.timeline({
+          onComplete() {
+            resolve();
+            current.container.remove();
+          }
+        })
+
+        timeline
+          .from('.page', {opacity: 1})
+          .to('.page', {opacity: 0});
+      })
+    },
+    enter({current, next, trigger}) {
+      return new Promise(resolve => {
+
+        const timeline = gsap.timeline({
+          onComplete() {
+            resolve();
+            // current.container.remove();
+          }
+        })
+
+        timeline
+          .from('.page', {opacity: 0})
+          .to('.page', {opacity: 1});
+      })
+    }
+  }],
+  views: [],
+  debug: true
+})
