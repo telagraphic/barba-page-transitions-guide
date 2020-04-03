02056b55ff7225bfa58f91cf6690d34d739ad85a
telagraphic <telagraphic@gmail.com>
2020-04-03 19:08:18 -0400

    🖥 FRONT-END GENERAL: used views with posts namespace to tweak before-enter/leave transitions

--- js/app.js
+++ js/app.js
@@ -37,36 +37,23 @@ barba.init({
           .to('.header__title', {opacity: 1, duration: .5}, 0)
       })
     }
-  },
-  {
-    name: 'posts-fade-in',
-    // from: {
-    //   namespace: [ 'index', 'about', 'links', 'gallery']
-    // },
-    to: {
-      namespace: ['posts']
-    },
-    beforeEnter({current, next, trigger}) {
-
+  }],
+  views: [{
+    namespace: 'posts',
+    afterEnter({current, next, trigger}) {
       return new Promise(resolve => {
 
         const timeline = gsap.timeline({
           onComplete() {
             resolve();
-            // next.container.remove();
           }
         })
 
         timeline
-          .from(current.container, {opacity:1})
-          .to(current.container, {opacity: 0})
+          .to('article.post', {opacity: 1, stagger: .1})
       })
     },
-    enter({current, next, trigger}) {
-      console.log("entering /posts");
-      const pageTitle = document.querySelector('.header__title');
-      pageTitle.innerText = next.container.getAttribute('data-title');
-
+    beforeLeave({current, next, trigger}) {
       return new Promise(resolve => {
 
         const timeline = gsap.timeline({
@@ -76,55 +63,7 @@ barba.init({
         })
 
         timeline
-          .to('article.post', {opacity: 1, delay: .1, stagger: .25}, )
-      })
-    }
-  },
-  {
-    name: 'posts-fade-out',
-    // from: {
-    //   namespace: ['posts']
-    // },
-    to: {
-      namespace: [ 'index', 'about', 'links', 'gallery']
-    },
-    leave({current, next, trigger}) {
-      console.log("leaving /posts");
-
-      return new Promise(resolve => {
-
-        const timeline = gsap.timeline({
-          onComplete() {
-            resolve();
-            current.container.remove();
-          }
-        })
-
-        timeline
-          .from(current.container, {opacity: 1})
-          .to(current.container, {opacity: 0, duration: .25})
-          .to('.post', {opacity: 0})
-
-      })
-    },
-    enter({current, next, trigger}) {
-      next.container.style.opacity = 0;
-      const pageTitle = document.querySelector('.header__title');
-      pageTitle.innerText = next.container.getAttribute('data-title');
-
-      return new Promise(resolve => {
-
-        const timeline = gsap.timeline({
-          onComplete() {
-            resolve();
-          }
-        })
-
-        timeline
-          .set('.header__title', {opacity: 0}, 0)
-          .set(next.container, {opacity: 0}, 0)
-          .to(next.container, {opacity: 1, duration: .5}, 0)
-          .to('.header__title', {opacity: 1, duration: .5}, 0)
+          .to('article.post', {opacity: 0, stagger: .1})
       })
     }
   }],
