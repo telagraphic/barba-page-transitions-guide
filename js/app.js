ac5b7921ae0c1f7a760edab5180adef3d1cf404f
telagraphic <telagraphic@gmail.com>
2020-04-03 18:38:45 -0400

    🖥 FRONT-END GENERAL: rules based on from/to create new barba-wrapper when transition to the custom route

--- js/app.js
+++ js/app.js
@@ -39,12 +39,54 @@ barba.init({
     }
   },
   {
-    name: 'posts-fade',
-    from: {
-      namespace: [ 'index', 'about', 'links', 'gallery']
-    },
+    name: 'posts-fade-in',
+    // from: {
+    //   namespace: [ 'index', 'about', 'links', 'gallery']
+    // },
     to: {
       namespace: ['posts']
+    },
+    beforeEnter({current, next, trigger}) {
+
+      return new Promise(resolve => {
+
+        const timeline = gsap.timeline({
+          onComplete() {
+            resolve();
+            // next.container.remove();
+          }
+        })
+
+        timeline
+          .from(current.container, {opacity:1})
+          .to(current.container, {opacity: 0})
+      })
+    },
+    enter({current, next, trigger}) {
+      console.log("entering /posts");
+      const pageTitle = document.querySelector('.header__title');
+      pageTitle.innerText = next.container.getAttribute('data-title');
+
+      return new Promise(resolve => {
+
+        const timeline = gsap.timeline({
+          onComplete() {
+            resolve();
+          }
+        })
+
+        timeline
+          .to('article.post', {opacity: 1, delay: .1, stagger: .25}, )
+      })
+    }
+  },
+  {
+    name: 'posts-fade-out',
+    // from: {
+    //   namespace: ['posts']
+    // },
+    to: {
+      namespace: [ 'index', 'about', 'links', 'gallery']
     },
     leave({current, next, trigger}) {
       console.log("leaving /posts");
@@ -59,17 +101,16 @@ barba.init({
         })
 
         timeline
-          .to('.post', {opacity: 0, stagger: .5})
-      })
+          .from(current.container, {opacity: 1})
+          .to(current.container, {opacity: 0, duration: .25})
+          .to('.post', {opacity: 0})
 
+      })
     },
     enter({current, next, trigger}) {
-      console.log("entering /posts");
-
-      // const posts = document.querySelectorAll('article.post');
-      // posts.forEach(post => {
-      //   post.style.opacity = 0;
-      // });
+      next.container.style.opacity = 0;
+      const pageTitle = document.querySelector('.header__title');
+      pageTitle.innerText = next.container.getAttribute('data-title');
 
       return new Promise(resolve => {
 
@@ -80,7 +121,10 @@ barba.init({
         })
 
         timeline
-          .to('article.post', {opacity: 1, delay: .1, stagger: .25}, )
+          .set('.header__title', {opacity: 0}, 0)
+          .set(next.container, {opacity: 0}, 0)
+          .to(next.container, {opacity: 1, duration: .5}, 0)
+          .to('.header__title', {opacity: 1, duration: .5}, 0)
       })
     }
   }],
