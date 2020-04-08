2f92bd26cd2d1ae3cc33c8059319f390aeb697d0
telagraphic <telagraphic@gmail.com>
2020-04-08 14:35:34 -0400

    🖥 FRONT-END GENERAL: setting up guide animations

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
+barba.init({
+  transitions: [
+    {
+      name: 'fade',
+      once({current, next, trigger}) {
+        return new Promise(resolve=> {
 
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+        })
+
+        
+      },
+      enter({current, next, trigger}) {},
+      leave({current, next, trigger}) {}
+    },
+  ],
+  debug: true
+})
