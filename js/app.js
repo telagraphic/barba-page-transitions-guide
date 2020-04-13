12c9816aff513f25ec934e38e0f3dd6ae95f5029
telagraphic <telagraphic@gmail.com>
2020-04-13 15:06:56 -0400

    🖥 FRONT-END GENERAL: finished flickering code example

--- js/app.js
+++ js/app.js
@@ -1,36 +1,10 @@
 barba.init({
   transitions: [
     {
-      name: "hooks",
-      before({current, next, trigger}) {
-        console.log('before');
-
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'before';
-
-        return new Promise(resolve => {
-
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-
-          timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
-        })
-      },
+      name: "hooks-flicker",
       beforeEnter({current, next, trigger}) {
         console.log('beforeEnter');
 
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'beforeEnter';
-
-        const hiliteScript = document.createElement('script');
-        hiliteScript.setAttribute('src', 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.5.0/prism.min.js');
-        next.container.appendChild(hiliteScript);
-
         return new Promise(resolve => {
 
           const timeline = gsap.timeline({
@@ -39,17 +13,15 @@ barba.init({
             }
           })
 
-          timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
+          // Uncomment to fix flickering
+          // timeline
+          //   .from(next.container, {opacity: 0})
         })
+
       },
       enter({current, next, trigger}) {
         console.log('enter');
 
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'enter';
-
         return new Promise(resolve => {
 
           const timeline = gsap.timeline({
@@ -59,72 +31,16 @@ barba.init({
           })
 
           timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
-        })
-      },
-      afterEnter({current, next, trigger}) {
-        console.log('afterEnter');
-
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'afterEnter';
-
-        return new Promise(resolve => {
-
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-
-          timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
+            .set(next.container, {opacity: 0}) // comment out
+            .to(next.container, {opacity: 1});
         })
       },
       beforeLeave({current, next, trigger}) {
         console.log('beforeLeave');
 
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'beforeLeave';
-
-        return new Promise(resolve => {
-
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-
-          timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
-        })
       },
       leave({current, next, trigger}) {
         console.log('leave');
-
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'leave';
-
-        return new Promise(resolve => {
-
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-
-          timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
-        })
-      },
-      afterLeave({current, next, trigger}) {
-        console.log('afterLeave');
-
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'afterLeave';
 
         return new Promise(resolve => {
 
@@ -136,33 +52,14 @@ barba.init({
           })
 
           timeline
-            .set(hooksHeader, {opacity: 0})
-            .to(hooksHeader, {opacity: 1, duration: 1});
+            .to(current.container, {opacity: 0});
         })
       },
-      after({current, next, trigger}) {
-        console.log('after');
+      afterLeave({current, next, trigger}) {
+        console.log('afterLeave');
 
-        let hooksHeader = document.querySelector('.hooks__header');
-        hooksHeader.textContent = 'after';
-
-        return new Promise(resolve => {
-
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-        })
       }
     }
   ],
   debug: true
-})
-
-// barba.hooks.beforeEnter( ( data ) => {
-//   const body = document.querySelector('body');
-//
-//   body.appendChild(hiliteScript);
-//
-// });
+});
