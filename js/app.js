0a2d8ed5032064199cd27672696341e624f97d3a
telagraphic <telagraphic@gmail.com>
2020-04-15 21:34:18 -0400

    ✨ NEW FEATURE: rules feature complete

--- js/app.js
+++ js/app.js
@@ -19,22 +19,18 @@ barba.init({
             .to('.github-button-section', {opacity: 1, y: '0%', duration: 1}, 0)
             .to('.page', {opacity: 1, y: 0}, 0);
         })
+      }
+    },
+    {
+      name: 'index-to-guide',
+      from: {
+          namespace: ['index', 'guide']
       },
-      enter({current, next, trigger}) {
-        return new Promise(resolve => {
-
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-
-          timeline
-            .from(next.container, {opacity: 0, y: 50})
-            .to(next.container, {opacity: 1, y: 0, ease: 'power4.out', duration: .25});
-        })
+      to: {
+          namespace: ['guide', 'index']
       },
       leave({current, next, trigger}) {
+        console.log('index-to-guide: leave');
         return new Promise(resolve => {
 
           const timeline = gsap.timeline({
@@ -45,14 +41,101 @@ barba.init({
           })
 
           timeline
-            .to(current.container, {opacity: 0, y: 50});
+            .to(current.container, {opacity: 0, x: 500, ease: 'circ'});
+        })
+      },
+      enter({current, next, trigger}) {
+        console.log('index-to-guide: enter');
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+
+          timeline
+            .from(next.container, {opacity: 0, x: -500})
+            .to(next.container, {opacity: 1, x: 0, ease: 'circ'});
         })
       }
     },
+    {
+      name: 'index-to-resources',
+      from: {
+          namespace: ['index', 'resources']
+      },
+      to: {
+          namespace: ['resources', 'index']
+      },
+      leave({current, next, trigger}) {
+        console.log('index-to-resources: leave');
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+              current.container.remove();
+            }
+          })
+
+          timeline
+            .to(current.container, {opacity: 0, x: -500, ease: 'circ'});
+        })
+      },
+      enter({current, next, trigger}) {
+        console.log('index-to-resources: enter');
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+
+          timeline
+            .from(next.container, {opacity: 0, x: 500})
+            .to(next.container, {opacity: 1, x: 0, ease: 'circ'});
+        })
+      }
+    },
+    {
+      name: 'to-guide',
+      to: {
+          namespace: ['guide']
+      },
+      leave({current, next, trigger}) {
+        console.log('to-guide: leave');
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+              current.container.remove();
+            }
+          })
+
+          timeline
+            .to(current.container, {opacity: 0, y: 500, ease: 'circ'});
+        })
+      },
+      enter({current, next, trigger}) {
+        console.log('to-guide: enter');
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+
+          timeline
+            .set(next.container, {overflow: 'hidden'})
+            .from(next.container, {opacity: 0, y: 500})
+            .to(next.container, {opacity: 1, y: 0, ease: 'circ'});
+        })
+      }
+    }
   ],
   debug: true
 })
-
-barba.hooks.after(() => {
-  fathom('set', 'spa', 'pushstate');
-});
