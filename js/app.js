ce5ab53069a4d2af8763fcbf3ae8b5df8601a421
telagraphic <telagraphic@gmail.com>
2020-04-22 14:22:12 -0400

    🚧 IN PROGRESS: playing around with sync mode code setup

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 function delay(n) {
   n = n || 2000;
   return new Promise(done => {
@@ -8,51 +7,42 @@ function delay(n) {
   });
 }
 
+function homeAnimation(next) {
+  const timeline = gsap.timeline();
+  timeline
+    .set(next.container, {opacity: 0})
+    .to(next.container, {opacity: 1, ease: 'circ'});
+}
+
+function leaveAnimation(current) {
+
+  const timeline = gsap.timeline();
+  timeline
+    .to(current.container, {opacity: 0, ease: 'circ'});
+
+}
+
 barba.init({
   transitions: [
     {
       sync: true,
       name: 'fade',
-      once({current, next, trigger}) {
-        return new Promise(resolve => {
+      async enter({current, next, trigger}) {
+        console.log('index-to-guide: enter');
 
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
-
-          timeline
-            .set('nav a', {opacity: 0, y: '-100%'})
-            .set('.github-button-section', {opacity: 0, y: '-100%'})
-            .set('.page', {opacity: 0, y: 50})
-            .to('nav a', {opacity: 1, stagger: .1, y: '0%', delay: .15}, 0)
-            .to('.github-button-section', {opacity: 1, y: '0%', duration: 1}, 0)
-            .to('.page', {opacity: 1, y: 0}, 0);
-        })
-      }
-    },
-    {
+        homeAnimation(next);
+      },
       async leave({current, next, trigger}) {
         console.log('index-to-guide: leave');
 
         const done = this.async();
 
-        const timeline = gsap.timeline();
-        timeline
-          .to(current.container, {opacity: 0, x: 500, ease: 'circ'});
+        leaveAnimation(current);
 
         await delay(1000);
         done();
+        // current.container.remove();
 
-      },
-      enter({current, next, trigger}) {
-        console.log('index-to-guide: enter');
-
-        const timeline = gsap.timeline();
-        timeline
-          .from(next.container, {opacity: 0, x: -500})
-          .to(next.container, {opacity: 1, x: 0, ease: 'circ'});
 
       }
     }
