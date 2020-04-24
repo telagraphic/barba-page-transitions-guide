98a5c926086e8ee34604436ffdf08d7ce4aef692
telagraphic <telagraphic@gmail.com>
2020-04-24 14:04:27 -0400

    🔥 REMOVED: synchronous copy

--- js/app.js
+++ js/app.js
@@ -1,43 +1,54 @@
-function delay(n) {
-  n = n || 2000;
-  return new Promise(done => {
-    setTimeout(() => {
-      done();
-    }, n);
-  });
-}
-
-function homeAnimation(next) {
-  const timeline = gsap.timeline();
-  timeline
-    .set(next.container, {opacity: 0})
-    .to(next.container, {opacity: 1, ease: 'circ'});
-}
-
-function leaveAnimation(current) {
-
-  const timeline = gsap.timeline();
-  timeline
-    .to(current.container, {opacity: 0, ease: 'circ'});
-
-}
-
 barba.init({
   transitions: [
     {
-      sync: true,
       name: 'fade',
+      once({current, next, trigger}) {
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+
+          timeline
+            .set('nav a', {opacity: 0, y: '-100%'})
+            .set('.github-button-section', {opacity: 0, y: '-100%'})
+            .set('.page', {opacity: 0, y: 50})
+            .to('nav a', {opacity: 1, stagger: .1, y: '0%', delay: .15}, 0)
+            .to('.github-button-section', {opacity: 1, y: '0%', duration: 1}, 0)
+            .to('.page', {opacity: 1, y: 0}, 0);
+        })
+      },
       enter({current, next, trigger}) {
-        const timeline = gsap.timeline();
-        timeline
-          .to(current.container, {opacity: 1, ease: 'circ'});
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+
+          timeline
+            .from(next.container, {opacity: 0, y: 50})
+            .to(next.container, {opacity: 1, y: 0, ease: 'power4.out', duration: .25});
+        })
       },
       leave({current, next, trigger}) {
-        const timeline = gsap.timeline();
-        timeline
-          .to(current.container, {opacity: 0, ease: 'circ'});
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+              current.container.remove();
+            }
+          })
+
+          timeline
+            .to(current.container, {opacity: 0, y: 50});
+        })
       }
-    }
+    },
   ],
   debug: true
-});
+})
