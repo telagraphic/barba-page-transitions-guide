6506b6617dab27ac890fcf4512eb8f04c8a8dbd8
telagraphic <telagraphic@gmail.com>
2020-04-24 14:52:43 -0400

    🖥 FRONT-END GENERAL: updated copy

--- js/app.js
+++ js/app.js
@@ -1,54 +1,43 @@
+function delay(n) {
+  n = n || 2000;
+  return new Promise(done => {
+    setTimeout(() => {
+      done();
+    }, n);
+  });
+}
+
 barba.init({
   transitions: [
     {
+      sync: true,
       name: 'fade',
-      once({current, next, trigger}) {
-        return new Promise(resolve => {
+      async enter({current, next, trigger}) {
+        console.log('fade: enter');
 
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
+        let done = this.async();
+        await delay(1000);
 
-          timeline
-            .set('nav a', {opacity: 0, y: '-100%'})
-            .set('.github-button-section', {opacity: 0, y: '-100%'})
-            .set('.page', {opacity: 0, y: 50})
-            .to('nav a', {opacity: 1, stagger: .1, y: '0%', delay: .15}, 0)
-            .to('.github-button-section', {opacity: 1, y: '0%', duration: 1}, 0)
-            .to('.page', {opacity: 1, y: 0}, 0);
-        })
+        const timeline = gsap.timeline();
+        timeline
+          .set(next.container, {opacity: 0})
+          .to(next.container, {opacity: 1, duration: 2, ease: 'circ'});
+
+        done();
       },
-      enter({current, next, trigger}) {
-        return new Promise(resolve => {
+      async leave({current, next, trigger}) {
+        console.log('fade: leave');
 
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-            }
-          })
+        let done = this.async();
 
-          timeline
-            .from(next.container, {opacity: 0, y: 50})
-            .to(next.container, {opacity: 1, y: 0, ease: 'power4.out', duration: .25});
-        })
-      },
-      leave({current, next, trigger}) {
-        return new Promise(resolve => {
+        const timeline = gsap.timeline();
+        timeline
+          .to(current.container, {opacity: 0, duration: 1, ease: 'circ'});
 
-          const timeline = gsap.timeline({
-            onComplete() {
-              resolve();
-              current.container.remove();
-            }
-          })
-
-          timeline
-            .to(current.container, {opacity: 0, y: 50});
-        })
+        await delay(1000);
+        done();
       }
-    },
+    }
   ],
   debug: true
 });
