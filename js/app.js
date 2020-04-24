5c255b640e898a46d00416a5f7fbe137f8ea4aec
telagraphic <telagraphic@gmail.com>
2020-04-24 13:57:59 -0400

    🖥 FRONT-END GENERAL: updated copy

--- js/app.js
+++ js/app.js
@@ -14,52 +14,20 @@ barba.init({
       async enter({current, next, trigger}) {
         console.log('fade: enter');
 
-        let done = this.async();
-
         const timeline = gsap.timeline();
         timeline
           .to(current.container, {opacity: 1, ease: 'circ'});
 
-        await delay(1000);
-        done();
-
-        // return new Promise(resolve => {
-        //   const timeline = gsap.timeline({
-        //     onComplete() {
-        //       resolve();
-        //
-        //     }
-        //   });
-        //
-        //   timeline
-        //     .from(next.container, {opacity: 0})
-        //     .to(next.container, {opacity: 1, ease: 'circ'});
-        // });
       },
       async leave({current, next, trigger}) {
         console.log('fade: leave');
-
-        let done = this.async();
 
         const timeline = gsap.timeline();
         timeline
           .to(current.container, {opacity: 0, ease: 'circ'});
 
-        await delay(2000);
         current.container.remove();
-        done();
 
-        // return new Promise(resolve => {
-        //   const timeline = gsap.timeline({
-        //     onComplete() {
-        //       resolve();
-        //       current.container.remove();
-        //     }
-        //   });
-        //
-        //   timeline
-        //     .to(current.container, {opacity: 0, ease: 'circ'});
-        // });
       }
     }
   ],
