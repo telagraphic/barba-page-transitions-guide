377e2aad5212c7f75b7745d744d6da6d61fbd9e4
telagraphic <telagraphic@gmail.com>
2020-04-24 13:51:17 -0400

    ✨ NEW FEATURE: tested all types of code

--- js/app.js
+++ js/app.js
@@ -7,35 +7,59 @@ function delay(n) {
   });
 }
 
-function homeAnimation(next) {
-  const timeline = gsap.timeline();
-  timeline
-    .set(next.container, {opacity: 0})
-    .to(next.container, {opacity: 1, ease: 'circ'});
-}
-
-function leaveAnimation(current) {
-
-  const timeline = gsap.timeline();
-  timeline
-    .to(current.container, {opacity: 0, ease: 'circ'});
-
-}
-
 barba.init({
   transitions: [
     {
-      sync: true,
       name: 'fade',
-      enter({current, next, trigger}) {
+      async enter({current, next, trigger}) {
+        console.log('fade: enter');
+
+        let done = this.async();
+
         const timeline = gsap.timeline();
         timeline
           .to(current.container, {opacity: 1, ease: 'circ'});
+
+        await delay(1000);
+        done();
+
+        // return new Promise(resolve => {
+        //   const timeline = gsap.timeline({
+        //     onComplete() {
+        //       resolve();
+        //
+        //     }
+        //   });
+        //
+        //   timeline
+        //     .from(next.container, {opacity: 0})
+        //     .to(next.container, {opacity: 1, ease: 'circ'});
+        // });
       },
-      leave({current, next, trigger}) {
+      async leave({current, next, trigger}) {
+        console.log('fade: leave');
+
+        let done = this.async();
+
         const timeline = gsap.timeline();
         timeline
           .to(current.container, {opacity: 0, ease: 'circ'});
+
+        await delay(2000);
+        current.container.remove();
+        done();
+
+        // return new Promise(resolve => {
+        //   const timeline = gsap.timeline({
+        //     onComplete() {
+        //       resolve();
+        //       current.container.remove();
+        //     }
+        //   });
+        //
+        //   timeline
+        //     .to(current.container, {opacity: 0, ease: 'circ'});
+        // });
       }
     }
   ],
