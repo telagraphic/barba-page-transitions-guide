4159fc73840c3cf3fe25282eaa239be047523d85
telagraphic <telagraphic@gmail.com>
2020-04-25 16:54:39 -0400

    ✨ NEW FEATURE: 5-interstitials feature setup

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const body = document.querySelector('body');
+
+const interstitial = document.createElement('section');
+interstitial.classList.add('interstitial');
+
+const interstitialHeader = document.createElement('h1');
+interstitialHeader.innerText = 'Transition';
+interstitial.appendChild(interstitialHeader);
+
+body.appendChild(interstitial);
+
 barba.init({
   transitions: [
     {
@@ -20,6 +31,23 @@ barba.init({
             .to('.page', {opacity: 1, y: 0}, 0);
         })
       },
+      beforeEnter({current, next, trigger}) {
+        return new Promise(resolve => {
+
+          const timeline = gsap.timeline({
+            onComplete() {
+              resolve();
+            }
+          })
+
+          timeline
+            .set(next.container, {opacity: 0, y: 50})
+            .to('.interstitial', {y: '100%', duration: 1, delay: 2})
+            .to('.interstitial h1', {opacity: 1}, 0)
+            .to('.interstitial h1', {opacity: 0}, 1.5);
+
+        })
+      },
       enter({current, next, trigger}) {
         return new Promise(resolve => {
 
@@ -30,8 +58,8 @@ barba.init({
           })
 
           timeline
-            .from(next.container, {opacity: 0, y: 50})
-            .to(next.container, {opacity: 1, y: 0, ease: 'power4.out', duration: .25});
+            .set(next.container, {opacity: 0, y: 50})
+            .to(next.container, {opacity: 1, y: 0, ease: 'circ', duration: .25});
         })
       },
       leave({current, next, trigger}) {
@@ -45,14 +73,11 @@ barba.init({
           })
 
           timeline
-            .to(current.container, {opacity: 0, y: 50});
+            .to(current.container, {opacity: 0, y: 50})
+            .to('.interstitial', {y: 0, duration: .5, ease: 'circ'});
         })
       }
     },
   ],
   debug: true
 })
-
-barba.hooks.after(() => {
-  fathom('set', 'spa', 'pushstate');
-});
